5c0d793c19bc3927b134828d7aaa3e8e54b10b29
Novaki <novakiart@gmail.com>
2021-10-12 14:51:29 -0300

    Novaki 12 de out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteModule } from './cliente/cliente.module';
+import { ProdutoModule } from './produto/produto.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { ClienteModule } from './cliente/cliente.module';
     BrowserModule,
     AppRoutingModule,
     ClienteModule,
+    ProdutoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/produto/services/produto.service.ts
+++ src/app/produto/services/produto.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
-
+import { Produto } from 'src/app/shared';
+const LS_CHAVE: string = "produtos";
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
+  
+  
   constructor() { }
+
+  listarTodos(): Produto[]{
+    const produtos = localStorage[LS_CHAVE];
+    return produtos ? JSON.parse(produtos) : [];
+  }
+
+   
+  inserir(produto: Produto): void{
+   
+    const produtos = this.listarTodos();
+    
+    produtos.push(produto);
+    
+    localStorage[LS_CHAVE] = JSON.stringify(produtos);
+  }
+
+  buscarPorId(id: number): Produto{
+    
+    const produtos: Produto[] = this.listarTodos();
+
+    return produtos.find(produto => produto.id_produto === id)!;
+    
+  }
+
+  atualizar(produto: Produto): void{
+    
+    const produtos: Produto[] = this.listarTodos();
+    console.log(produto)
+   
+    produtos.forEach( (obj, index, objs) => {
+      if(produto.id_produto === obj.id_produto){
+        objs[index] = produto
+      }
+    });
+    
+    localStorage[LS_CHAVE] = JSON.stringify(produtos);
+  }
+
+  remover(id: number): void{
+   
+    let produtos: Produto[] = this.listarTodos();
+    
+    produtos = produtos.filter(produto => produto.id_produto !== id);
+
+    localStorage[LS_CHAVE] = JSON.stringify(produtos)
+  }
+
+
+
 }
