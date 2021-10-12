3dcee4f092057090b70df34d6cd13549915707a0
R4M05 <49047862+R4M05@users.noreply.github.com>
2021-10-12 14:00:32 -0300

    Changing "listar pedido component" as a list of products
    Changing "listar pedido component" as a list of products

--- src/app/pedido/listar-pedido/listar-pedido.component.ts
+++ src/app/pedido/listar-pedido/listar-pedido.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/shared/models/produto.model';
 
 @Component({
   selector: 'app-pedido',
   templateUrl: './listar-pedido.component.html',
   styleUrls: ['./listar-pedido.component.css']
 })
+
 export class ListarPedidoComponent implements OnInit {
+
+  produtos: Produto[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    this.produtos= [ 
+      new Produto (12345,"Produto 12345"), 
+      new Produto (12346,"Produto 12346"), 
+      new Produto (12347,"Produto 12347")];
+      
+  }
 
 }
