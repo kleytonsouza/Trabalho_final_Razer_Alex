b30fbebaefe0f17298c013f3f4cc25f60102aeab
Sergio <sergiohcosta@gmail.com>
2021-10-25 19:55:25 -0300

    configurando e testado jest

--- FrontAngular/src/app/shared/models/cliente.model.spec.ts
+++ FrontAngular/src/app/shared/models/cliente.model.spec.ts
@@ -1,7 +1,28 @@
-import { Cliente } from './cliente.model';
+import { Produto } from './produto.model';
+import { ItemDoPedido } from './itemdopedido.model';
+import { Pedido } from './cliente.model';
+import { Cliente } from './cliente';
+
+/*import { Cliente } from './cliente.model';
 
 describe('Cliente', () => {
   it('should create an instance', () => {
     expect(new Cliente()).toBeTruthy();
   });
 });
+*/
+
+test('testa cliente.model', () => {
+
+  let cliente = new Cliente(1,"04767743907","Sergio","Costa");
+  let data = new Date();
+  let produto = new Produto(1,"Produto");
+  let item = new ItemDoPedido(1,produto,cliente);
+  let itens = [
+    item = new ItemDoPedido(1,produto,cliente)
+  ];
+
+  expect(new Pedido(data,cliente,itens)).toBeDefined();;
+
+});
+
