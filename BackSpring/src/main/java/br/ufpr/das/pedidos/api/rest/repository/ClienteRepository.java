d58947c5213a828971b5dffe2162a9151bb3dcdd
NovakiDouglas <72577071+NovakiDouglas@users.noreply.github.com>
2021-11-15 22:33:39 -0300

    Add query find by CPF

--- BackSpring/src/main/java/br/ufpr/das/pedidos/api/rest/repository/ClienteRepository.java
+++ BackSpring/src/main/java/br/ufpr/das/pedidos/api/rest/repository/ClienteRepository.java
@@ -1,6 +1,8 @@
 package br.ufpr.das.pedidos.api.rest.repository;
 
 import br.ufpr.das.pedidos.api.rest.model.ClienteModel;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -9,4 +11,9 @@ public interface ClienteRepository extends CrudRepository<ClienteModel, Integer>
 
 
     Optional<ClienteModel> findByCpf(Integer integer);
+
+    @Query("SELECT t FROM cliente t WHERE cpf = ?1")
+    Iterable<ClienteModel> findAllByCpf(String cpf);
+
+
 }
